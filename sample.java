31c283bc90d1ad4b0183c13960f001b347dd4abf
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-28 21:10:38 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,3 +5,48 @@
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    List<List<Character>> blocks = new ArrayList<>();
+    List<String> result = new ArrayList<>();
+    public String[] expand(String s) {
+        int i=0;
+        while(i < s.length()) {
+            char c = s.charAt(i);
+            List<Character> block = new ArrayList<>();
+            if (c == '{') {
+                i++;
+                while (s.charAt(i) != '}') {
+                    if (s.charAt(i) != ',') {
+                        block.add(s.charAt(i));
+                    }
+                    i++;
+                }
+            } else {
+                block.add(c);
+            }
+            Collections.sort(block);
+            System.out.println(block);
+            blocks.add(block);
+            i++;
+        }
+        backtrack(0, new StringBuilder());
+        String[] answer = new String[result.size()];
+        for (int j=0;j<result.size();j++) {
+            answer[j] = result.get(j);
+        }
+        return answer;
+    }
+    private void backtrack(int index, StringBuilder sb) {
+        if (index == blocks.size()) {
+            result.add(new StringBuilder(sb).toString());
+            return;
+        }
+        List<Character> chlist = blocks.get(index);
+        for (int i=0;i<chlist.size();i++) {
+            int length = sb.length();
+            sb.append(chlist.get(i));
+            backtrack(index+1, sb);
+            sb.setLength(length);
+        }
+    }
+}
